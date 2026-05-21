f9283486f778576c650fe77130fe043836cfc411
Laetetia <laetetiamumbe@gmail.com>
2026-05-21 19:38:31 +0300

    Portfolio v4: typing animation and UI polish

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio loaded ");
+const text = "Hi, I'm Laetetia ";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.querySelector(".hero h1").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 80);
+    }
+}
+
+window.onload = typeEffect;
